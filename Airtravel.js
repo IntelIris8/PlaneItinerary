5749b048d38ec3856d5cde719129172dc2f0a318
YoSoyJesseFuerte <fortejesse@yahoo.com>
2018-01-10 13:20:10 -0800

    Plane Itinerary

---
+++ Airtravel.js
@@ -0,0 +1,17 @@
+function displayData(data) {
+  var retInfo;
+  for (resultIndex in data.results) {
+    retInfo += '<div class="resultBox">';
+    var result = data.results[resultIndex];
+    for (dataItem in result) {
+      retInfo += dataItem + ':  ' + result[dataItem] + ' <br>';
+    }
+    retInfo += "</div>";
+  }
+  $(".results").html(retInfo);
+}
+
+$.get("https://api.myjson.com/bins/1gb9tf", function(data) {
+  displayData(data);
+  console.log(data);
+});
